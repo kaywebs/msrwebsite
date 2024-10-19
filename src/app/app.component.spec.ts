7108566bc81dc4f66ab0f8725b884a34af27e94f
kaywebs <tywitcow@gmail.com>
2024-10-19 00:07:11 -0700

    rdg

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'msrwebsite' title`, () => {
+  it(`should have the 'my-angular-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('msrwebsite');
+    expect(app.title).toEqual('my-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, msrwebsite');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-app');
   });
 });
